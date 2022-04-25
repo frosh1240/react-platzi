b0e27d5de3cef0beb71a6c575e76dbf24a567e03
frosh1240 <camr2669@gmail.com>
2022-04-25 15:40:42 -0500

    second

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,21 +2,28 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./styles/TodoItem.css";
 import { faTrashArrowUp, faCheck } from "@fortawesome/free-solid-svg-icons";
+import { Card } from "react-bootstrap";
 
 function TodoItem(props) {
   return (
     <li className="TodoItems">
-      <span
-        className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}
-      >
-        <FontAwesomeIcon icon={faCheck} />
-      </span>
-      <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
-        {props.text}
-      </p>
-      <span className="Icon Icon-delete">
-        <FontAwesomeIcon icon={faTrashArrowUp} />
-      </span>
+      <Card body style={{ width: "18rem" }}>
+        <span
+          className={`Icon Icon-check ${
+            props.completed && "Icon-check--active"
+          }`}
+        >
+          <FontAwesomeIcon icon={faCheck} />
+        </span>
+        <p
+          className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}
+        >
+          {props.text}
+        </p>
+        <span className="Icon Icon-delete">
+          <FontAwesomeIcon icon={faTrashArrowUp} />
+        </span>
+      </Card>
     </li>
   );
 }
